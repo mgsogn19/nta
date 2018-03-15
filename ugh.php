9f4588ae8a0bcfb3f5825683a2fcded24aebdf6f
mgsogn19 <37390260+mgsogn19@users.noreply.github.com>
2018-03-15 11:44:58 +0900

    Delete ugh.php

--- ugh.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $con = mysql_connect("localhost", "smg6135", "iamagod1026");
-    mysql_select_db("smg6135", $con);
-    $mysql_query = "SELECT userPoint FROM USER WHERE userID like 'smg6135'";
-    $result = mysqli_query($con, $mysql_query);
-    
-    echo ($result);
-    mysqli_close($con);
-?>
